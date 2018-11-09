35595c418ed2040034028603caea7c3365fe19ee
Rémi Alvergnat <toilal.dev@gmail.com>
2018-11-09 23:23:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Docker Compose Spec TypeScript
+===============================================
+
+![npm](https://img.shields.io/npm/v/@gfi-centre-ouest/docker-compose-spec-typescript.svg)
+
+Docker Compose configuration file schemas as TypeScript interfaces.
+
+Package generated from [JSONSchema files of official Docker Compose project](https://github.com/docker/compose/tree/master/compose/config),
+thanks to [bcherny/json-schema-to-typescript](https://github.com/bcherny/json-schema-to-typescript).
---
+++ build.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosResponse } from 'axios'
+import { compile } from 'json-schema-to-typescript'
+import * as rimraf from 'rimraf'
+import { writeFileSync } from 'fs'
+import { join } from 'path'
+import * as fs from 'fs'
+
+const directory = 'lib'
+const schemaUrlPattern: string = 'https://raw.githubusercontent.com/docker/compose/master/compose/config/config_schema_v{version}.json'
+const schemaVersions: string[] = ['1', '2.0', '2.1', '2.2', '2.3', '2.4', '3.0', '3.1', '3.2', '3.3', '3.4', '3.5', '3.6', '3.7']
+
+rimraf.sync(directory)
+fs.mkdirSync(directory)
+
+const promises: Promise<{ compile: string, version: string }>[] = []
+for (const version of schemaVersions) {
+  const url = schemaUrlPattern.replace('{version}', version)
+  promises.push(axios.get(url).then((response: AxiosResponse) => {
+    return { compilePromise: compile(response.data, `docker-compose-spec-v${version}.ts`), version }
+  }).then((compilePromiseAndVersion: { compilePromise: Promise<string>, version: string }) => {
+    return compilePromiseAndVersion.compilePromise.then((compile: string) => {
+      return { compile, version }
+    })
+  }).then((compileAndVersion: { compile: string, version: string }) => {
+    writeFileSync(join(directory, `docker-compose-spec-v${version}.ts`), compileAndVersion.compile)
+    return compileAndVersion
+  }))
+}
+
+Promise.all(promises).then((resolved: { compile: string, version: string }[]) => {
+  console.log(`${resolved.length} files written in ${directory}.`)
+}).catch((err) => {
+  console.error(err)
+  process.exit(-1)
+})
+
+
+
+
+
+
+
+
